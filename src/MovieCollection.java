17b8bc48167ca0ba64c29e1cef8a18d6e11694d4
BT_1N3_18 <jackeyh2@Nycstudents.net>
2024-02-16 09:28:33 -0500

    finished title

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -21,7 +21,8 @@ public class MovieCollection {
         for (String movie: stringData) {
             String[] split = movie.split(",");
             movies.add(new Movie(split[0], split[1], split[2], split[3], Integer.valueOf(split[4]), Double.valueOf(split[5])));
-        } mainMenu();
+        }
+        mainMenu();
     }
 
     public void mainMenu() {
@@ -39,8 +40,7 @@ public class MovieCollection {
             if (menuOption.equals("t")) {
                 searchTitles();
             } else if (menuOption.equals("c")) {
-                System.out.println("empty right now");
-//                searchCast();
+                searchCast();
             } else if (menuOption.equals("q")) {
                 System.out.println("Goodbye!");
             } else {
@@ -52,18 +52,40 @@ public class MovieCollection {
     public void searchTitles() {
         System.out.print("Enter a title search term: ");
         String search = scanner.nextLine();
-        ArrayList<String> titles = new ArrayList<>();
+        ArrayList<Movie> titles = new ArrayList<>();
         for (Movie movie: movies) {
-            if (movie.getTitle().contains(search)) {
-                titles.add(movie.getTitle());
+            if (movie.getTitle().toLowerCase().contains(search)) {
+                titles.add(movie);
             }
-        } if (titles.size() == 0) {
+        } titles = sortMovieTitles(titles);
+        if (titles.size() == 0) {
             System.out.println("No movie titles match that search term!");
         } else {
             for (int i = 0; i < titles.size(); i++) {
-                System.out.println(i+1 + ". " + titles.get(i));
+                System.out.println(i+1 + ". " + titles.get(i).getTitle());
             }
+            System.out.print("Which movie would you like to learn more about? ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("\nTitle: " + titles.get(choice-1).getTitle() + "\nRuntime: " + titles.get(choice-1).getRuntime() + " minutes\nDirected by: " + titles.get(choice-1).getDirector() + "\nCast: " + titles.get(choice-1).getCast() + "\nOverview: " + titles.get(choice-1).getOverview() + "\nUser Rating: " + titles.get(choice-1).getUserRating());
         }
+    }
+
+    private ArrayList<Movie> sortMovieTitles(ArrayList<Movie> titles) {
+        for (int i = 1; i < titles.size(); i++) {
+            Movie current = titles.get(i);
+            String currentTitle = titles.get(i).getTitle();
+            int index = i;
+            while (index > 0 && currentTitle.compareTo(titles.get(index-1).getTitle()) < 0) {
+                titles.set(index, titles.get(index-1));
+                index--;
+            }
+            titles.set(index, current);
+        } return titles;
+    }
+
+    public void searchCast() {
+        System.out.print("Enter a person to search for (first or last name: ");
 
     }
 
